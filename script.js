1f2dd1401aec5a9daac7eed42207cd95feea6047
ParaLuisa <santiaustralia46@gmail.com>
2025-02-11 13:57:15 -0400

    segunto commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const boton_no = document.querySelector(".boton_no")
 
 const boton_si = document.querySelector(".boton_si")
 
-let valor = 80
+let valor = 20
 
 function agrandar(){
     valor = valor * 1.25
